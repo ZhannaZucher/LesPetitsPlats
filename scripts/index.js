6ead1348a928e053ff278090beff10d761848085
ZhannaZucher <jzucher@gmail.com>
2023-03-20 09:49:28 +0100

    add : unselect tags

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,14 @@
 import { recipes } from "../data/recipes.js";
 import { RecipeCardDOM } from "./templates/recipeCardDOM.js";
 import { buildTagsDOM } from "./templates/tagsContainer.js";
-import { displayTag } from "./utils/tagsEvents.js";
+import { displayTag, unselectTags } from "./utils/tagsEvents.js";
 
 function init() {
 	const RecipesTemplate = new RecipeCardDOM(recipes);
 	RecipesTemplate.buildCardDOM();
 	buildTagsDOM();
 	displayTag();
+	unselectTags();
 }
 
 init()
