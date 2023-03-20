4ebd4e843ee689b3c56caac47574e6c8fb8cc42d
ZhannaZucher <jzucher@gmail.com>
2023-03-20 08:33:59 +0100

    add: search on selected tags

--- scripts/utils/tagsEvents.js
+++ scripts/utils/tagsEvents.js
@@ -1,19 +1,22 @@
+import { state } from "./searchEvents.js";
+import { render, search } from "./searchEngine.js";
+
 /**
  * permet de construire le DOM pour le tag selectionné
  * @param {string} tagtype type de liste de tags : ingrédients, appliances, ustensils
  * @param {string} tag 
  *  @param {string} filterList liste de propriétés à filtrer, séparées par virgule 
  */
-const buildTagTemplate = (tagtype, tag, filterList) => {
+const buildTagTemplate = (tagtype, tag) => {
 	const closeIcon = document.createElement("img");
 	closeIcon.setAttribute("src", "/assets/close.svg");
 	closeIcon.setAttribute("alt", "icon");
 	closeIcon.classList.add("closeBtn");
 
+	tag.setAttribute("filter-tag-list", tagtype);
 	tag.classList.add(tagtype);
 	tag.classList.add("selectedTag");
 	tag.appendChild(closeIcon);
-	tag.setAttribute("search-filter", filterList)
 	document.querySelector(".tag-container").appendChild(tag);
 }
 
@@ -22,20 +25,27 @@ const buildTagTemplate = (tagtype, tag, filterList) => {
  */
 export function displayTag() {
 	const tagList = document.querySelectorAll(".filter__list li");
+
 	tagList.forEach((item) => item.addEventListener("click", (event) => {
 	const tag = event.target.cloneNode(true);
 
 	switch (item.closest("ul").id) {
 		case "list-ingredients": 
-			buildTagTemplate("tag-ingredients", tag, "recipes.ingredients.ingredient");
+			buildTagTemplate("tag-ingredients", tag);
+			state.setFilter("ingredientsSelectedTags", event.target.innerText);
 			break;
 		case "list-appliances":
-			buildTagTemplate("tag-appliances", tag, "recipes.appliance");
+			buildTagTemplate("tag-appliances", tag);
+			state.setFilter("appliancesSelectedTags", event.target.innerText);
 			break;
 		case "list-ustensils":
-			buildTagTemplate("tag-ustensils", tag, "recipes.ustensils");
+			buildTagTemplate("tag-ustensils", tag);
+			state.setFilter("ustensilsSelectedTags", event.target.innerText);
 			break;
-		}				
+		}	
+		
+		let recipeIdList = search();
+		render(recipeIdList);
 	}));
 }
 
@@ -46,10 +56,24 @@ function unselectTags() {
 	const selectedTagsBar = document.querySelector(".tag-container");
 	selectedTagsBar.addEventListener("click", function(event) {
 		if ( event.target.closest(".closeBtn")) {
-			console.log(event.target.closest);
 			const unselectTag = event.target.parentNode;
 			selectedTagsBar.removeChild(unselectTag);
 			unselectTag.classList.remove("selectedTag");
+
+			switch (unselectTag.getAttribute("filter-tag-list")) {
+				case "tag-ingredients":
+					state.unsetFilterByValue("ingredientsSelectedTags", unselectTag.innerText);
+					break;
+				case "tag-appliances":
+					state.unsetFilterByValue("appliancesSelectedTags", unselectTag.innerText);
+					break;
+				case "tag-ustensils":
+					state.unsetFilterByValue("ustensilsSelectedTags", unselectTag.innerText);
+					break;
+			}
+
+			let recipeIdList = search();
+			render(recipeIdList);
 		}
 	})
 }
