bc5af80719f3611a0587d233ca97320b889717c7
ZhannaZucher <jzucher@gmail.com>
2023-03-20 08:36:26 +0100

    fix: object state properties

--- scripts/utils/searchEvents.js
+++ scripts/utils/searchEvents.js
@@ -1,4 +1,5 @@
 import { render, search } from "./searchEngine.js";
+import { buildTagsDOM } from "../templates/tagsContainer.js";
 /*
 TODO:
 listeners on
@@ -16,32 +17,65 @@ export const state = {
 		ingredientsSelectedTags: [],
 		appliancesSelectedTags: [],
 		ustensilsSelectedTags: [],
-		keyword: "",
+		keywords: [],
 	},
-	setFilter: function() {
+	getFilterTypeList: function (filterName) {
+		if (this.filters.hasOwnProperty(filterName)) {
+			return this.filters[filterName];
+		}
 
+		return [];
+	},
+	setFilter: function (filterName, keyword) {
+		if (this.filters.hasOwnProperty(filterName)) {
+			if (filterName === "keywords") {
+				this.filters[filterName][0] = keyword;
+			} else {
+				this.filters[filterName].push(keyword.toLowerCase());
+			}
+		}
 	},
 	getFilters: function () {
 		return this.filters;
 	},
 	isFilterSet: function () {
-		return this.ingredientsSelectedTags.length !== 0
-			|| this.appliancesSelectedTags.length !== 0
-			|| this.ingredientsSelectedTags.length !== 0
-			|| this.keyword.length >= 3;
+		let keyword = this.filters.keywords[0] ?? "" ;
+		return this.filters.ingredientsSelectedTags.length !== 0
+			|| this.filters.appliancesSelectedTags.length !== 0
+			|| this.filters.ingredientsSelectedTags.length !== 0
+			|| keyword.length >= 3;
+	},
+	unsetFilterByValue: function (filterName, filterValue) {
+		if (this.filters.hasOwnProperty(filterName)) {
+			let filterIndex = this.filters[filterName].indexOf(filterValue);
+			if (filterIndex !== -1) {
+				this.filters[filterName].splice(filterIndex, 1);
+			}
+		}
 	}
 }
 
+// écoute de l'événement sur l'input de recherche principale
 mainSearchInput.addEventListener("input", function (event) {
-	state.keyword = event.target.value.toLowerCase().replace(/\s/g, ""); //remplacement d'espaces par chaînes de charactères vides
-	if (state.keyword.length >= 3) {
+	//remplacement d'espaces par chaînes de charactères vides
+	let searchTerm = event.target.value.toLowerCase().replace(/\s/g, ""); 
+	state.setFilter("keyword", searchTerm);
+	if (searchTerm.length >= 3) {
 		let recipeIdList = search();
 		render(recipeIdList);
 	}
-	if (state.keyword.length < 3) {
+	if (searchTerm.length < 3) {
 		render([]);
 	}
 })
 
-
-
+// écoute de l'événement sur les inputs de recherche des tags
+tagSearchInputs.forEach((input) => input.addEventListener("input", (event) => {
+	let tagKeyword = event.target.value.toLowerCase().replace(/\s/g, "");
+	if (tagKeyword.length >= 3) {
+		//search tags and display matches
+		console.log("coucou");
+	} else {
+		buildTagsDOM();
+	}
+}))
